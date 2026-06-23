df55fd607586f97793560d47abb0b0c7b3bf259e
Diksha Aggarwal <dikshaaggarwal.in08@gmail.com>
2026-06-23 15:12:52 +0200

    Refactor calculate_sum_and_product function for clarity and consistency

--- Sprint-1/Python/calculate_sum_and_product/calculate_sum_and_product.py
+++ Sprint-1/Python/calculate_sum_and_product/calculate_sum_and_product.py
@@ -12,20 +12,20 @@ def calculate_sum_and_product(input_numbers: List[int]) -> Dict[str, int]:
         "sum": 10, // 2 + 3 + 5
         "product": 30 // 2 * 3 * 5
     }
-    Time Complexity:
-    Space Complexity:
-    Optimal time complexity:
+    Time Complexity:O(n)
+    Space Complexity: O(1)
+    Optimal time complexity: O(n)
     """
     # Edge case: empty list
+   
     if not input_numbers:
         return {"sum": 0, "product": 1}
 
-    sum = 0
-    for current_number in input_numbers:
-        sum += current_number
-
+    total_sum = 0
     product = 1
+
     for current_number in input_numbers:
+        total_sum += current_number
         product *= current_number
 
-    return {"sum": sum, "product": product}
+    return {"sum": total_sum, "product": product}
