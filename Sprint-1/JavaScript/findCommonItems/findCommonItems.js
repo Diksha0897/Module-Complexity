3d4a2b98c1caf641bc52b3d0f7719d1a1812bf6e
Diksha Aggarwal <dikshaaggarwal.in08@gmail.com>
2026-06-23 14:58:26 +0200

    Refactor findCommonItems to improve time complexity from O(n * m) to O(n + m)

--- Sprint-1/JavaScript/findCommonItems/findCommonItems.js
+++ Sprint-1/JavaScript/findCommonItems/findCommonItems.js
@@ -1,14 +1,20 @@
 /**
  * Finds common items between two arrays.
  *
- * Time Complexity:
- * Space Complexity:
- * Optimal Time Complexity:
+ * Time Complexity:  O(n + m)
+ * Space Complexity: O(m)
+ * Optimal Time Complexity: O(n + m)
  *
  * @param {Array} firstArray - First array to compare
  * @param {Array} secondArray - Second array to compare
  * @returns {Array} Array containing unique common items
  */
-export const findCommonItems = (firstArray, secondArray) => [
+/*export const findCommonItems = (firstArray, secondArray) => [
   ...new Set(firstArray.filter((item) => secondArray.includes(item))),
-];
+];*/
+
+export const findCommonItems = (firstArray, secondArray) => {
+  const secondSet = new Set(secondArray);
+
+  return [...new Set(firstArray.filter((item) => secondSet.has(item)))];
+};
