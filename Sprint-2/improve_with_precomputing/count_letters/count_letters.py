53e085771b19af3958d1dc9de128aa18650b4a87
Diksha Aggarwal <dikshaaggarwal.in08@gmail.com>
2026-06-29 17:08:00 +0200

    Enhance count_letters function to filter non-alphabetic characters in uppercase check

--- Sprint-2/improve_with_precomputing/count_letters/count_letters.py
+++ Sprint-2/improve_with_precomputing/count_letters/count_letters.py
@@ -1,14 +1,16 @@
-def count_letters(s: str) -> int:
-    """
-    count_letters returns the number of letters which only occur in upper case in the passed string.
-    """
-    only_upper = set()
-    for letter in s:
-        if is_upper_case(letter):
-            if letter.lower() not in s:
-                only_upper.add(letter)
-    return len(only_upper)
+ def count_letters(s: str) -> int:
+     """
+     count_letters returns the number of letters which only occur in upper case in the passed string.
+     """
+     only_upper = set()
+     for letter in s:
+-        if is_upper_case(letter):
++        if is_upper_case(letter) and letter.isalpha():
+             if letter.lower() not in s:
+                 only_upper.add(letter)
+     return len(only_upper)
 
 
-def is_upper_case(letter: str) -> bool:
-    return letter == letter.upper()
+ def is_upper_case(letter: str) -> bool:
+-    return letter == letter.upper()
++    return letter.isalpha() and letter == letter.upper()
